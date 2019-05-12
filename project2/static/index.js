ca883f950217cba05fc10053def77963f57355d5
StevenGillis <s.gillis40@gmail.com>
2019-05-12 21:18:55 +0300

    klaar project2

--- project2/static/index.js
+++ project2/static/index.js
@@ -9,8 +9,9 @@
         console.log('socketIO is connected');
         document.querySelector('#chatsubmit').onsubmit = () => {
         message = document.querySelector('#chattext').value;
+        channel = document.querySelector('#currentchannel').textContent;
         usernameSend = "placeholder for username"
-        socket.emit('submit message', {"message": message, "usernameSend": usernameSend});
+        socket.emit('submit message', {"message": message, "channel": channel,"usernameSend": usernameSend});
         document.querySelector('#chattext').value = '';
         return false;
         };
@@ -19,9 +20,15 @@
     //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     //When a chat message has been emited by other user I wan't to register this
     socket.on('announce message', data => {
-        const li = document.createElement('li');
-        li.innerHTML = `${data.usernameSend}: ${data.message}`;
-        document.querySelector('#chathistory').append(li);
+        currentchannel = document.querySelector('#currentchannel').textContent;
+        senderchannel = `${data.channel}`;
+        console.log(senderchannel);
+        console.log(currentchannel);
+        if (currentchannel == senderchannel) {
+            const li = document.createElement('li');
+            li.innerHTML = `${data.usernameSend}: ${data.message}`;
+            document.querySelector('#chathistory').append(li);
+            }
         return false;
     });
     /*
